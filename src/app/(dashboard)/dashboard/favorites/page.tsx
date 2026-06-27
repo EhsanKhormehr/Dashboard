0346a7dc29959b1b042d9809c603a3b303ae52da
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-06-27 21:11:11 +0330

    feat: create favorites page

---
+++ src/app/(dashboard)/dashboard/favorites/page.tsx
@@ -0,0 +1,34 @@
+import PageHeader from "@/components/common/page-header";
+import { Input } from "@/components/ui/input";
+import ProductsCard from "@/features/dashboard/products/components/products-card";
+import ProductsFilter from "@/features/dashboard/products/components/products-filter";
+import ProductsPagination from "@/features/dashboard/products/components/products-pagination";
+import { Search } from "lucide-react";
+import React from "react";
+
+export default function Favorites() {
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <PageHeader title="Favorites" />
+        <ProductsFilter />
+      </div>
+      <div className="relative w-full sm:w-[260px] md:w-[350px] mt-6">
+        <Input
+          type="text"
+          placeholder="Search..."
+          className="bg-surface rounded-3xl pl-10 text-sm text-foreground py-5"
+        />
+        <Search className="absolute top-1/2 text-foreground opacity-35 bottom-0 left-2 -translate-y-1/2" />
+      </div>
+      <div className="mt-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
+          <ProductsCard />
+          <ProductsCard />
+          <ProductsCard />
+        </div>
+        <ProductsPagination />
+      </div>
+    </div>
+  );
+}
