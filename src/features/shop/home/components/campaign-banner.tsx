b8a5097b57affea722d19a74600019a3831b672e
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-07-18 18:03:22 +0330

    fix: fix campaign style

--- src/features/shop/home/components/campaign-banner.tsx
+++ src/features/shop/home/components/campaign-banner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const HomePageCampaignBanner = () => {
   return (
-    <div className="relative h-[450px] bg-gradient-to-r from-zinc-700 to-neutral-900">
+    <div className="relative h-[450px] bg-gradient-to-r from-zinc-700 to-neutral-900 text-center">
       <div className=" absolute inset-0 bg-cover bg-center bg-no-repeat bg-[url(/shop/campaign-banner.png)]"></div>
       <div className="flex flex-col items-center justify-center z-10 relative h-full">
         <span className="text-6xl text-white">
@@ -14,7 +14,7 @@ const HomePageCampaignBanner = () => {
         </span>
         <Link
           href={"/"}
-          className="text-white py-3 inline-block mt-10 text-sm px-14 border font-semibold rounded-sm"
+          className="text-white py-4 inline-block mt-10 text-sm px-14 border font-semibold rounded-sm"
         >
           Shop Now
         </Link>
