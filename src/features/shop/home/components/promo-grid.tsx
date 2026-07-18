34dce789b0d2ae9daa90d5adc9fe1265ffbb51e5
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-07-18 12:16:32 +0330

    fix: add slider to promo section

--- src/features/shop/home/components/promo-grid.tsx
+++ src/features/shop/home/components/promo-grid.tsx
@@ -1,14 +1,53 @@
-
+"use client";
 import React from "react";
 import HomePagePromoGridCard from "./promo-grid-card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 const HomePagePromoGrid = () => {
   return (
-    <div className="grid grid-cols-4 mb-16">
-      <HomePagePromoGridCard />
-      <HomePagePromoGridCard />
-      <HomePagePromoGridCard />
-      <HomePagePromoGridCard />
+    <div className="mb-16">
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 2000,
+            stopOnInteraction : false
+          }),
+        ]}
+        opts={{
+          align: "end",
+        }}
+      >
+        <CarouselContent>
+          <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0">
+            <HomePagePromoGridCard />
+          </CarouselItem>
+          <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0">
+            <HomePagePromoGridCard />
+          </CarouselItem>
+          <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0">
+            <HomePagePromoGridCard />
+          </CarouselItem>
+          <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0">
+            <HomePagePromoGridCard />
+          </CarouselItem>
+          <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0">
+            <HomePagePromoGridCard />
+          </CarouselItem>
+          <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0">
+            <HomePagePromoGridCard />
+          </CarouselItem>
+          <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0">
+            <HomePagePromoGridCard />
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
     </div>
   );
 };
