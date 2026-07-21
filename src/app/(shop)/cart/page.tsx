d01a1c53641477498002e148dae6adb63a52af9e
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-07-21 18:35:08 +0330

    feat: develop cart page

---
+++ src/app/(shop)/cart/page.tsx
@@ -0,0 +1,12 @@
+import CartWrapper from '@/features/shop/cart/components/cart-wrapper'
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <CartWrapper />
+    </div>
+  )
+}
+
+export default Cart
