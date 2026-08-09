db08f5d1c9ddc0d32b16404113cc4a5b7c1b89bc
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-09 19:45:48 +0330

    refactor: add condition for shape shop title

--- src/components/common/shop-title.tsx
+++ src/components/common/shop-title.tsx
@@ -3,11 +3,21 @@ import React from "react";
 
 type ShopTitleProps = {
   title: string;
-  className? : string
+  className?: string;
+  isShape?: boolean;
 };
 
-const ShopTitle = ({ title , className }: ShopTitleProps) => {
-  return <span className={cn("text-xl font-bold block relative before:absolute pl-5 before:w-[14px] before:rounded-md before:h-[5px] before:bg-primary before:content-[''] before:top-1/2 before:-translate-y-1/2 before:left-0" , className)}>{title}</span>;
+const ShopTitle = ({ title, className, isShape = true }: ShopTitleProps) => {
+  return (
+    <span
+      className={cn(
+        `text-xl font-bold block relative ${isShape && "before:absolute before:w-[14px] before:rounded-md before:h-[5px] before:bg-primary before:content-[''] before:top-1/2 before:-translate-y-1/2 before:left-0 pl-5"}`,
+        className,
+      )}
+    >
+      {title}
+    </span>
+  );
 };
 
 export default ShopTitle;
