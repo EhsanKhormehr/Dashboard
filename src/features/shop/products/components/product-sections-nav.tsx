4924403ad2440d653ddc242f0f3cd21714ffa5e9
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-09 20:00:59 +0330

    style(shop/product): fix responsive design

--- src/features/shop/products/components/product-sections-nav.tsx
+++ src/features/shop/products/components/product-sections-nav.tsx
@@ -3,17 +3,17 @@ import React from "react";
 const ProductSectionsNav = () => {
   return (
     <div className="mt-5 bg-surface shadow-soft-card rounded-xl overflow-hidden text-muted-foreground">
-      <div className="flex ml-5">
+      <div className="flex ml-2 sm:ml-5 text-sm sm:text-base overflow-x-auto text-nowrap">
         <a
-          className="mr-7 py-4 border-b-3 border-primary text-surface-foreground font-bold"
+          className="mr-3 sm:mr-7 py-4 border-b-3 border-primary text-surface-foreground font-bold"
           href="#specifications"
         >
           Specifications
         </a>
-        <a className="mr-7 py-4" href="#expert-review">
+        <a className="mr-3 sm:mr-7 py-4" href="#expert-review">
           Expert Review
         </a>
-        <a className="mr-7 py-4" href="#reviews">
+        <a className="mr-3 sm:mr-7 py-4" href="#reviews">
           Reviews
         </a>
       </div>
