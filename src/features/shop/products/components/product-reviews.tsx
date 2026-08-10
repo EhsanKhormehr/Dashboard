5c29773f50933c3b3c3e9f082cd33f849bb9e86a
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-10 14:10:50 +0330

    refactor(shop/product): link comments section to sections nav

--- src/features/shop/products/components/product-reviews.tsx
+++ src/features/shop/products/components/product-reviews.tsx
@@ -4,7 +4,7 @@ import ProductsReviewsWrapper from "./product-reviews-wrapper";
 
 const ProductReviews = () => {
   return (
-    <div className="bg-surface p-5 col-span-12 lg:col-span-9 rounded-xl mt-2 shadow-soft-card">
+    <div className="bg-surface p-5 col-span-12 lg:col-span-9 rounded-xl mt-2 shadow-soft-card" id="reviews">
       <ProductReviewForm />
       <ProductsReviewsWrapper />
     </div>
