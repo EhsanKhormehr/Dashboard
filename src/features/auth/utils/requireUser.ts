94e9a6087ae3bc480d6289b19cf9c63d1eecce84
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-13 19:30:01 +0330

    feat(auth): develop requiure user

--- src/features/auth/utils/requireUser.ts
+++ src/features/auth/utils/requireUser.ts
@@ -1,8 +1,9 @@
 import { getCurrentUser } from "./getCurrentUser";
 
 export const requireUser = async () => {
-  const currentUser = await getCurrentUser()
+  const currentUser = await getCurrentUser();
   if (!currentUser) {
-    throw new Error("Unauthorized!")
+    throw new Error("Unauthorized!");
   }
+  return currentUser;
 };
