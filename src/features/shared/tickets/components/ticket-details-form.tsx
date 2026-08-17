aaf7fa8f4a97f36b490d4565ea40f7ea52b3c5b5
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-17 16:52:58 +0330

    refactor(dashboard/tickets): convert textarea message box to controlled textarea

--- src/features/shared/tickets/components/ticket-details-form.tsx
+++ src/features/shared/tickets/components/ticket-details-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { Controller, useForm } from "react-hook-form";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import React from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -13,7 +13,12 @@ import {
 } from "@/features/dashboard/tickets/services/useMutation";
 import { useReplyTicket } from "@/features/account/tickets/services/useMutation";
 import { TicketStatus } from "../../../../../generated/prisma/enums";
-import { detailsTicketDefaultValues, DetailsTicketFormValues, detailsTicketSchema } from "../types/schema";
+import {
+  detailsTicketDefaultValues,
+  DetailsTicketFormValues,
+  detailsTicketSchema,
+} from "../types/schema";
+import ControlledTextarea from "@/components/common/controlled-textarea";
 
 type TicketDetailsFormProps = {
   ticketId: string;
@@ -26,14 +31,15 @@ const TicketDetailsForm = ({
   role,
   status,
 }: TicketDetailsFormProps) => {
+  const form = useForm<DetailsTicketFormValues>({
+    resolver: zodResolver(detailsTicketSchema),
+    defaultValues: detailsTicketDefaultValues,
+  });
   const {
     control,
     formState: { errors },
     handleSubmit,
-  } = useForm<DetailsTicketFormValues>({
-    resolver: zodResolver(detailsTicketSchema),
-    defaultValues: detailsTicketDefaultValues,
-  });
+  } = form;
   const { mutate: replyUser } = useReplyTicket();
   const { mutate: replyAdmin } = useAdminReplyTicket();
   const { mutate: closeTicket } = useCloseTicket();
@@ -59,36 +65,40 @@ const TicketDetailsForm = ({
     return null;
   }
   return (
-    <form className="py-5" onSubmit={handleSubmit(sendTicketSubmitHandler)}>
-      <Controller
-        control={control}
-        name="message"
-        render={({ field }) => (
-          <Textarea
-            value={field.value}
-            onChange={(event) => field.onChange(event.target.value)}
-            className="bg-background h-[200px]"
-            placeholder="Enter your message..."
-          ></Textarea>
-        )}
-      />
-      {errors.message && <ErrorMessage text={errors.message.message} />}
-      <div className="flex justify-end">
-        {role === "ADMIN" && (
-          <Button
-            type="button"
-            variant={status === "OPEN" || status === "ANSWERED" || status === "PENDING" ? "destructive" : "outline"}
-            className="mt-5 px-10 py-5 cursor-pointer mr-3"
-            onClick={toggleTicketStatusHandler}
-          >
-            {status === "OPEN" || status === "ANSWERED" || status === "PENDING" ? "Close Ticket" : "Open Ticket"}
+    <FormProvider {...form}>
+      <form className="py-5" onSubmit={handleSubmit(sendTicketSubmitHandler)}>
+        <ControlledTextarea<DetailsTicketFormValues>
+          name="message"
+          className="bg-background h-[200px]"
+          placeholder="Enter your message..."
+        />
+        <div className="flex justify-end">
+          {role === "ADMIN" && (
+            <Button
+              type="button"
+              variant={
+                status === "OPEN" ||
+                status === "ANSWERED" ||
+                status === "PENDING"
+                  ? "destructive"
+                  : "outline"
+              }
+              className="mt-5 px-10 py-5 cursor-pointer mr-3"
+              onClick={toggleTicketStatusHandler}
+            >
+              {status === "OPEN" ||
+              status === "ANSWERED" ||
+              status === "PENDING"
+                ? "Close Ticket"
+                : "Open Ticket"}
+            </Button>
+          )}
+          <Button type="submit" className="mt-5 px-10 py-5 cursor-pointer">
+            Send
           </Button>
-        )}
-        <Button type="submit" className="mt-5 px-10 py-5 cursor-pointer">
-          Send
-        </Button>
-      </div>
-    </form>
+        </div>
+      </form>
+    </FormProvider>
   );
 };
 
