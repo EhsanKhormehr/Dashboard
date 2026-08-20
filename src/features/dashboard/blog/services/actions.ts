59d6ea34d98d8856f81c7fe654b86fd8356f1b78
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-20 17:16:31 +0330

    feat(dashboard/feat): develop create blog action

---
+++ src/features/dashboard/blog/services/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { executeAction } from "@/lib/executeAction";
+import { BlogFormValues, newBlogSchema } from "../types/schema";
+import { requireAdmin } from "@/features/auth/utils/requireAdmin";
+import { prisma } from "@/lib/prisma";
+
+export const createBlog = async (data: BlogFormValues) => {
+  return executeAction({
+    actionFn: async () => {
+      const currentAdmin = await requireAdmin();
+      const validatedData = newBlogSchema.parse(data);
+
+      return await prisma.blog.create({
+        data: {
+          title: validatedData.title,
+          slug: validatedData.slug,
+          description: validatedData.description,
+          category: validatedData.category,
+          thumbnail: validatedData.thumbnail ?? null,
+          content: validatedData.content,
+          userId: currentAdmin.id,
+        },
+      });
+    },
+  });
+};
