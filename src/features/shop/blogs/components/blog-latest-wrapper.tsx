7b333071d6d3ef3291e68567ca5870a3a343c947
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-25 22:15:58 +0330

    feat(shop/blogs): develop latest wrapper

---
+++ src/features/shop/blogs/components/blog-latest-wrapper.tsx
@@ -0,0 +1,27 @@
+import { Newspaper } from "lucide-react";
+import BlogLatestCard from "./blog-latest-card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const BlogLatestWrapper = () => {
+  return (
+    <div className="bg-surface rounded-xl shadow-soft-card px-4">
+      <div className="flex gap-2 py-5">
+        <Newspaper />
+        <span className="font-extrabold">Latest Articles</span>
+      </div>
+      <div className="pb-5 flex flex-col gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4">
+          <BlogLatestCard />
+          <BlogLatestCard />
+          <BlogLatestCard />
+        </div>
+        <Button asChild variant={"outline"}>
+          <Link href={"/"}>View All</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default BlogLatestWrapper;
