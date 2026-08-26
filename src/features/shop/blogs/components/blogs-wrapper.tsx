bdb01d20af3a7fe865d02e0243e413b8568005b7
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-26 22:37:18 +0330

    feat(shop/blogs): show blogs from db and dynamic orderby url

--- src/features/shop/blogs/components/blogs-wrapper.tsx
+++ src/features/shop/blogs/components/blogs-wrapper.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -19,20 +20,44 @@ import { Filter, Search } from "lucide-react";
 import React from "react";
 import BlogsFilter from "./blogs-filter";
 import BlogsCard from "./blogs-card";
+import { useUpdateUrlParams } from "@/hooks/use-update-url-params";
+import { Prisma } from "../../../../../generated/prisma/client";
 
-const BlogsWrapper = () => {
+type BlogsWrapperProps = {
+  articles: Prisma.BlogGetPayload<{
+    include: {
+      user: {
+        select: {
+          userName: true;
+        };
+      };
+    };
+  }>[];
+};
+
+const BlogsWrapper = ({ articles }: BlogsWrapperProps) => {
+  const { updateParam } = useUpdateUrlParams();
   return (
     <div className="">
       <div className="grid grid-cols-2 lg:flex lg:items-center w-full lg:w-auto gap-5">
         <div>
-          <Select>
+          <Select
+            defaultValue="LATEST"
+            onValueChange={(value) => {
+              updateParam({
+                key: "sortBy",
+                value: value,
+                defaultValue: "LATEST",
+              });
+            }}
+          >
             <SelectTrigger className="bg-surface py-6 px-4 w-full">
               <SelectValue placeholder="Sort By" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
-                <SelectItem value="newest">Newest</SelectItem>
-                <SelectItem value="oldest">Oldest</SelectItem>
+                <SelectItem value="LATEST">Latest</SelectItem>
+                <SelectItem value="OLDEST">Oldest</SelectItem>
               </SelectGroup>
             </SelectContent>
           </Select>
@@ -57,13 +82,24 @@ const BlogsWrapper = () => {
         </Sheet>
       </div>
       <div className="mt-6 grid grid-cols-1 min-[410]:grid-cols-2 md:grid-cols-3 gap-5">
-        <BlogsCard />
-        <BlogsCard />
-        <BlogsCard />
-        <BlogsCard />
+        {articles.map((article) => (
+          <BlogsCard
+            createdAt={article.createdAt}
+            description={article.description}
+            slug={article.slug}
+            title={article.title}
+            userName={article.user?.userName}
+            key={article.id}
+          />
+        ))}
       </div>
       <div className="flex justify-center">
-        <Button variant={"outline"} className="h-11 px-20 cursor-pointer mt-6 font-bold ">Load More</Button>
+        <Button
+          variant={"outline"}
+          className="h-11 px-20 cursor-pointer mt-6 font-bold "
+        >
+          Load More
+        </Button>
       </div>
     </div>
   );
