ac0f6f047812698c177e32c53a2cf1a0519df59c
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-26 17:40:17 +0330

    feat(shop/blogs): dynamic latest blog card

--- src/features/shop/blogs/components/latest-blog-card.tsx
+++ src/features/shop/blogs/components/latest-blog-card.tsx
@@ -4,29 +4,52 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const LatestBlogCard = () => {
+type LatestBlogCardProps = {
+  id: string;
+  title: string;
+  category: string;
+  slug: string;
+  createdAt: Date;
+};
+
+const LatestBlogCard = ({
+  title,
+  category,
+  id,
+  slug,
+  createdAt,
+}: LatestBlogCardProps) => {
   return (
     <div className="flex items-center rounded-md overflow-hidden">
-      <div className="hidden md:flex">
-        <Link href={"/"}>
+      <div className="hidden md:flex w-1/3 h-auto lg:w-[280px] lg:h-[170px]">
+        <Link href={`/blogs/${slug}`}>
           <Image
             src={"/shop/blog-1.jpg"}
-            width={280}
-            height={170}
+            width={1000}
+            height={1000}
             alt="blog"
-            className="object-contain rounded-xl"
+            className="w-full h-auto lg:w-[280px] lg:h-[170px] object-cover rounded-xl "
           />
         </Link>
       </div>
       <div className="flex flex-col justify-between h-full ml-3">
-        <div className="inline-flex self-start rounded-full text-xs border-primary/70 border-1 px-4 py-1 text-primary bg-blue-100 font-bold dark:bg-primary/10 dark:text-blue-400">
-          Gaming
-        </div>
-        <Link href={"/"} className="text-base md:text-lg font-extrabold my-3">
-          How to Choose the Best Gaming Keyboard in 2026
+        <Link
+          href={`/blogs/categories/${category}`}
+          className="inline-flex self-start rounded-full text-xs border-primary/70 border-1 px-4 py-1 text-primary bg-blue-100 font-bold dark:bg-primary/10 dark:text-blue-400 capitalize"
+        >
+          {category}
+        </Link>
+        <Link
+          href={`/blogs/${slug}`}
+          className="text-base md:text-lg font-extrabold my-3"
+        >
+          {title}
         </Link>
         <div className="self-end">
-          <Link href={"/"} className="flex items-center text-sm hover:text-primary transition duration-300 font-bold">
+          <Link
+            href={`/blogs/${slug}`}
+            className="flex items-center text-sm hover:text-primary transition duration-300 font-bold"
+          >
             Read More
             <ArrowRight className="size-4 ml-1" />
           </Link>
