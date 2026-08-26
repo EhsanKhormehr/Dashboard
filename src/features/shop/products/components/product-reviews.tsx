474c636a112ef771211fc5b0b30e222347ebc22b
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-26 11:02:59 +0330

    refactor(shop/comments): refactor product reviews comments

--- src/features/shop/products/components/product-reviews.tsx
+++ src/features/shop/products/components/product-reviews.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ProductReviewForm from "./product-review-form";
-import ProductsReviewsWrapper from "./product-reviews-wrapper";
+import ReviewForm from "../../shared/components/comments/review-form";
+import ReviewsWrapper from "../../shared/components/comments/reviews-wrapper";
 
 const ProductReviews = () => {
   return (
     <div className="bg-surface p-5 col-span-12 lg:col-span-9 rounded-xl mt-2 shadow-soft-card" id="reviews">
-      <ProductReviewForm />
-      <ProductsReviewsWrapper />
+      <ReviewForm />
+      <ReviewsWrapper />
     </div>
   );
 };
