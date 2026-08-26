1053bca47ed1495b6d0d7eb2e9b7bba19b4213ea
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-26 12:31:52 +0330

    refactor(shop/blogs): use commentsprovider form comments wrapper

--- src/features/shop/shared/components/comments/comments-wrapper.tsx
+++ src/features/shop/shared/components/comments/comments-wrapper.tsx
@@ -2,31 +2,38 @@ import React from "react";
 import { Star } from "lucide-react";
 import CommentsBox from "./comments-box";
 import CommentsForm from "./comments-form";
+import { CommentsProvider } from "./comments-context";
 
-const CommentsWrapper = () => {
+type CommentsWrapperProps = {
+  type: "product" | "blog";
+};
+
+const CommentsWrapper = ({ type }: CommentsWrapperProps) => {
   return (
-    <div>
-      <CommentsForm />
-      <div className="mt-10">
-        <div className="border-b flex items-center justify-between">
-          <div className="py-2">
-            <span className="text-base font-bold text-surface-foreground">
-              Reviews
-            </span>
-            <span className="text-muted-foreground text-xs ml-1">(110)</span>
+    <CommentsProvider type={type}>
+      <div>
+        <CommentsForm />
+        <div className="mt-10">
+          <div className="border-b flex items-center justify-between">
+            <div className="py-2">
+              <span className="text-base font-bold text-surface-foreground">
+                Reviews
+              </span>
+              <span className="text-muted-foreground text-xs ml-1">(110)</span>
+            </div>
+            <div className="flex items-center gap-1 rounded-full border border-yellow-400/50 dark:border-yellow-400/20 bg-yellow-400/10 px-3 py-1 text-xs font-semibold text-rating">
+              <Star className="size-3.5 fill-rating stroke-rating" />
+              4.8 average
+            </div>
           </div>
-          <div className="flex items-center gap-1 rounded-full border border-yellow-400/50 dark:border-yellow-400/20 bg-yellow-400/10 px-3 py-1 text-xs font-semibold text-rating">
-            <Star className="size-3.5 fill-rating stroke-rating" />
-            4.8 average
+          <div>
+            <CommentsBox />
+            <CommentsBox />
+            <CommentsBox />
           </div>
-        </div>
-        <div>
-          <CommentsBox />
-          <CommentsBox />
-          <CommentsBox />
         </div>
       </div>
-    </div>
+    </CommentsProvider>
   );
 };
 
