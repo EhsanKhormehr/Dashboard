01f06412fac9350ffb6ca02c93884dd9e4b78428
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-26 22:43:00 +0330

    feat(shop/blogs): develop get articles action by params

--- src/features/shop/blogs/services/actions.ts
+++ src/features/shop/blogs/services/actions.ts
@@ -2,6 +2,12 @@
 
 import { executeAction } from "@/lib/executeAction";
 import { prisma } from "@/lib/prisma";
+import { Prisma } from "../../../../../generated/prisma/client";
+
+type getArticlesParams = {
+  search?: string;
+  sortBy?: string;
+};
 
 export const getLatestArticles = async () => {
   return executeAction({
@@ -13,12 +19,45 @@ export const getLatestArticles = async () => {
           title: true,
           createdAt: true,
           category: true,
-          slug : true
+          slug: true,
         },
         orderBy: {
           createdAt: "desc",
         },
         take: 4,
+      });
+    },
+  });
+};
+
+export const getArticles = async (params: getArticlesParams) => {
+  return executeAction({
+    actionFn: async () => {
+      const where: Prisma.BlogWhereInput = {};
+      const orderBy: Prisma.BlogOrderByWithRelationInput = {
+        createdAt: "desc",
+      };
+      if (params.search && params.search !== "") {
+        where.title = {
+          contains: params.search,
+          mode: "insensitive",
+        };
+      }
+
+      if (params.sortBy && params.sortBy !== "LATEST") {
+        orderBy.createdAt = "asc";
+      }
+
+      return await prisma.blog.findMany({
+        where,
+        orderBy,
+        include: {
+          user: {
+            select: {
+              userName: true,
+            },
+          },
+        },
       });
     },
   });
