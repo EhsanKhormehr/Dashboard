5f10b7e29547e58255c259de6bebfbc93cbfc914
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-26 22:35:49 +0330

    feat(shop/blogs): dynamic url by search params

--- src/features/shop/blogs/components/blogs-filter.tsx
+++ src/features/shop/blogs/components/blogs-filter.tsx
@@ -1,9 +1,10 @@
-import ShopTitle from "@/components/common/shop-title";
+"use client";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Field, FieldGroup } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { useDebouncedSearchParams } from "@/hooks/use-debounced-search-params";
 import { LayoutGrid, Search } from "lucide-react";
 import React from "react";
 
@@ -20,16 +21,21 @@ const BLOG_CATEGORIES = [
 ];
 
 const BlogsFilter = () => {
+  const [searchValue, setSearchValue] = useDebouncedSearchParams();
   return (
     <div>
-      <form className="relative hidden lg:flex">
+      <div className="relative hidden lg:flex">
         <Input
           placeholder="Search articles..."
           type="text"
           className="h-13 pl-10 border-0 !bg-surface shadow-soft-card"
+          value={searchValue}
+          onChange={(event) => {
+            setSearchValue(event.target.value);
+          }}
         />
         <Search className="absolute top-1/2 left-3 -translate-y-1/2 h-full text-muted-foreground" />
-      </form>
+      </div>
       <div className="lg:mt-6 bg-surface rounded-lg p-4 shadow-soft-card">
         <div className="flex items-center border-b pb-2">
           <LayoutGrid className="size-5" />
@@ -38,9 +44,22 @@ const BlogsFilter = () => {
         <ScrollArea className="h-70 py-4">
           <FieldGroup>
             {BLOG_CATEGORIES.map((category) => (
-              <Field orientation={"horizontal"} key={category.label} className="flex items-center ">
-                <Checkbox id={category.label} name={category.label} className="[&_svg]:hidden cursor-pointer" />
-                <Label htmlFor={category.label} className="font-bold w-full cursor-pointer">{category.label}</Label>
+              <Field
+                orientation={"horizontal"}
+                key={category.label}
+                className="flex items-center "
+              >
+                <Checkbox
+                  id={category.label}
+                  name={category.label}
+                  className="[&_svg]:hidden cursor-pointer"
+                />
+                <Label
+                  htmlFor={category.label}
+                  className="font-bold w-full cursor-pointer"
+                >
+                  {category.label}
+                </Label>
               </Field>
             ))}
           </FieldGroup>
