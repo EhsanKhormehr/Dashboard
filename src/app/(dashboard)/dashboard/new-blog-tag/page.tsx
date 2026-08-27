3883cda97e0b8851dd2ed7fe7879ff34d7eebb4d
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-27 19:33:00 +0330

    feat(dashboard/blogs): develop new blog tag page

---
+++ src/app/(dashboard)/dashboard/new-blog-tag/page.tsx
@@ -0,0 +1,22 @@
+import PageHeader from "@/components/common/page-header";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import BlogTagForm from "@/features/dashboard/blog/components/blog-tag-form";
+import React from "react";
+
+const NewTag = () => {
+  return (
+    <div>
+      <PageHeader title="New Blog Tag" />
+      <Card className="shadow-card mt-6">
+        <CardHeader className="flex items-center justify-between gap-2 space-y-0 py-0 sm:flex-row">
+          <CardTitle className="font-bold text-2xl">New Blog Tag</CardTitle>
+        </CardHeader>
+        <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+          <BlogTagForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default NewTag;
