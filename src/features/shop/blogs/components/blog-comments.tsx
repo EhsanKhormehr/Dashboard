a1733cc002d35b216208b7347a33e3e5095d2632
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-30 11:24:41 +0330

    feat(shop/blogs): pass userid and blogid to commentsWrapper component

--- src/features/shop/blogs/components/blog-comments.tsx
+++ src/features/shop/blogs/components/blog-comments.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import CommentsWrapper from "../../shared/components/comments/comments-wrapper";
 import { TextAlignStart } from "lucide-react";
 
-const BlogComments = () => {
+type BlogCommentsProps = {
+  userId: string | null;
+  blogId: string;
+};
+
+const BlogComments = ({ userId, blogId }: BlogCommentsProps) => {
   return (
     <div className="bg-surface p-5 rounded-xl shadow-soft-card">
       <div className="flex items-center gap-2">
         <TextAlignStart className="text-primary" />
         <h3 className="font-extrabold text-lg">Comments</h3>
       </div>
-      <CommentsWrapper type="blog" />
+      <CommentsWrapper type="blog" userId={userId} blogId={blogId} />
     </div>
   );
 };
