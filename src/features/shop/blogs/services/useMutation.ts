bb4a3b42b53f9a90d428c31e297e8ba87f6a3039
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-08-30 11:22:51 +0330

    feat(shop/blogs): develop submitArticleComment hook

--- src/features/shop/blogs/services/useMutation.ts
+++ src/features/shop/blogs/services/useMutation.ts
@@ -1,7 +1,14 @@
 "use client";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { getLatestArticles } from "./actions";
+import { getLatestArticles, submitArticleComment } from "./actions";
+import { toast } from "sonner";
+
+type SubmitArticleCommentVaribles = {
+  content: string;
+  blogId: string;
+  userId: string;
+};
 
 export const useGetLatestArticles = () => {
   const queryClient = useQueryClient();
@@ -9,6 +16,20 @@ export const useGetLatestArticles = () => {
     mutationFn: getLatestArticles,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["blog"] });
+    },
+  });
+};
+
+export const useSubmitArticleComment = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ content, blogId, userId }: SubmitArticleCommentVaribles) =>
+      submitArticleComment(content, blogId, userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["blog"] });
+      toast.success(
+        "Comment submitted successfully. It will be displayed after approval by an administrator.",
+      );
     },
   });
 };
