c04a7cb6f1da85134dd09eea9df8a14ffe214238
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-01 17:54:06 +0330

    refactor(products): remove optional from thumbnail blog

--- src/features/dashboard/blog/types/schema.ts
+++ src/features/dashboard/blog/types/schema.ts
@@ -17,7 +17,7 @@ export const newBlogSchema = z.object({
       /^[a-z0-9]+(?:-[a-z0-9]+)*$/,
       "Slug must contain only English letters, numbers, and hyphens",
     ),
-  thumbnail: z.any().optional(),
+  thumbnail: z.any(),
   content: z.any(),
   description: z.string().trim().min(1, "Description field is required!"),
   category: z.string().min(1, "Category filed is required"),
