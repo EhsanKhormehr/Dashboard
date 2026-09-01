78e0e9e49c50088dd7d72f96ac0afde0de6db9c6
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-01 13:47:22 +0330

    reafctor(blogs): convert default uploader to reusable uploader

--- src/features/dashboard/blog/components/blog-form.tsx
+++ src/features/dashboard/blog/components/blog-form.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
-import React, { useState } from "react";
 import { Controller, FormProvider, useForm } from "react-hook-form";
 import {
   blogFormDefaultValues,
@@ -12,9 +11,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import ControlledInput from "@/components/common/controlled-input";
 import { Button } from "@/components/ui/button";
 import BlogTextEditor from "./blog-text-editor";
-import { Plus, UploadCloud } from "lucide-react";
+import { Plus } from "lucide-react";
 import ControlledTextarea from "@/components/common/controlled-textarea";
-import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
@@ -39,7 +37,7 @@ import {
   ComboboxValue,
   useComboboxAnchor,
 } from "@/components/ui/combobox";
-import Image from "next/image";
+import ImageUploader from "@/components/common/image-uploader";
 
 const BLOG_CATEGORIES = [
   { value: "hardware", label: "Hardware" },
@@ -69,23 +67,12 @@ const BlogForm = ({ mode, initialValue, blogId }: BlogFormProps) => {
   const { mutate: updateBlog } = useUpdateBlog();
   const { data: blogTags } = useGetBlogTags();
   const anchor = useComboboxAnchor();
-  const [preview, setPreview] = useState<string | undefined>("");
+
   const normalizeThumbnail = (thumbnail: string) => {
     return `/${thumbnail.replace(/^\/+/, "")}`;
   };
-  const imageChangeHandler = (
-    event: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
-  ) => {
-    const file = event.target.files?.[0];
-    if (!file) return;
-
-    const previewImage = URL.createObjectURL(file);
-    setPreview(previewImage);
-  };
 
   const newBlogSubmitHandler = (data: BlogFormValues) => {
-    const imageUrl = normalizeThumbnail(data.thumbnail);
-    console.log(imageUrl);
     if (mode === "create") {
       createBlog({
         title: data.title,
@@ -96,7 +83,7 @@ const BlogForm = ({ mode, initialValue, blogId }: BlogFormProps) => {
         slug: data.slug,
         status: data.status,
         tags: data.tags,
-        thumbnail: imageUrl,
+        thumbnail: data.thumbnail,
       });
     }
     if (mode === "edit") {
@@ -114,7 +101,7 @@ const BlogForm = ({ mode, initialValue, blogId }: BlogFormProps) => {
           slug: data.slug,
           status: data.status,
           tags: data.tags,
-          thumbnail: imageUrl,
+          thumbnail: data.thumbnail,
         },
       });
     }
@@ -269,64 +256,11 @@ const BlogForm = ({ mode, initialValue, blogId }: BlogFormProps) => {
             </Field>
           </FieldGroup>
           <FieldGroup className="mb-6 ">
-            <FieldLabel>Thumbnail</FieldLabel>
-            <Controller
-              control={control}
-              name="thumbnail"
-              render={({ field, fieldState: { error } }) => (
-                <>
-                  <Label htmlFor="upload" className="inline">
-                    <div className="group border-2 border-dashed border-muted-foreground/30 p-8 rounded-xl  cursor-pointer hover:border-primary hover:bg-primary/5 transition-all duration-200">
-                      {initialValue?.thumbnail && !preview && (
-                        <div className="flex h-[360px] items-center justify-center overflow-hidden">
-                          <Image
-                            src={initialValue.thumbnail}
-                            width={1000}
-                            height={1000}
-                            alt="Blog thumbnail preview"
-                            className="h-full w-full object-contain "
-                          />
-                        </div>
-                      )}
-                      {preview && (
-                        <div className="flex h-[360px] items-center justify-center overflow-hidden">
-                          <Image
-                            src={preview}
-                            width={1000}
-                            height={1000}
-                            alt="Blog thumbnail preview"
-                            className="h-full w-full object-contain "
-                          />
-                        </div>
-                      )}
-                      {!preview && !initialValue?.thumbnail && (
-                        <div className="flex flex-col justify-center items-center">
-                          <div className="p-3 bg-muted/50 rounded-full group-hover:bg-primary/20 transition-colors duration-200">
-                            <UploadCloud className="size-8 text-muted-foreground group-hover:text-primary" />
-                          </div>
-                          <span className="font-semibold text-muted-foreground mt-3 group-hover:text-primary">
-                            Browse file to upload
-                          </span>
-                          <span className="text-xs text-muted-foreground/70 mt-1">
-                            PNG, JPG or WebP (Max 5MB)
-                          </span>
-                        </div>
-                      )}
-                      <input
-                        type="file"
-                        className="hidden"
-                        onChange={(event) => {
-                          field.onChange(event.target.files?.[0].name);
-                          imageChangeHandler(event);
-                        }}
-                        id="upload"
-                        accept="image/*"
-                      />
-                    </div>
-                    {error && <ErrorMessage text={error.message} />}
-                  </Label>
-                </>
-              )}
+            <ImageUploader<BlogFormValues>
+              initImage={initialValue?.thumbnail}
+              name={"thumbnail"}
+              id="uploader"
+              label="Thumbnail"
             />
           </FieldGroup>
           <FieldGroup>
