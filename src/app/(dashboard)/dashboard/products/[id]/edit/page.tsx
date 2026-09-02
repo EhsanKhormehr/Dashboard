3075b7035d410ff52ad982ea1591f15f5013e354
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-02 17:37:43 +0330

    feat(products): develop edit product page

--- src/app/(dashboard)/dashboard/products/[id]/edit/page.tsx
+++ src/app/(dashboard)/dashboard/products/[id]/edit/page.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import PageHeader from "@/components/common/page-header";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import ProductForm from "@/features/dashboard/products/components/product-form";
+import { getProductById } from "@/features/dashboard/products/services/actions";
+import React from "react";
 
-export default function ProductEdit() {
+type ProductEditProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function ProductEdit({ params }: ProductEditProps) {
+  const urlParams = await params;
+  const productId = urlParams.id;
+  const product = await getProductById(productId);
+  if (!product) return;
+  const initialProduct = {
+    name: product.name,
+    slug: product.slug,
+    price: product.price,
+    stock: product.stock,
+    content: product.content,
+    categoryId: product.categoryId,
+    thumbnail: product.thumbnail,
+    images: product.images,
+    description: product.description ?? "",
+  };
+
+  const initialValue = {
+    ...initialProduct,
+    attributes: Object.fromEntries(
+      product.attributes.map((item) => [item.attribute.slug, item.value]),
+    ),
+  };
   return (
-    <div>ProductEdit</div>
-  )
+    <div>
+      <PageHeader title="New Product" />
+      <Card className="shadow-card mt-6">
+        <CardHeader className="flex items-center justify-between gap-2 space-y-0 py-0 sm:flex-row">
+          <CardTitle className="font-bold text-2xl">New Product</CardTitle>
+        </CardHeader>
+        <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+          <ProductForm
+            mode="edit"
+            productId={productId}
+            initialValue={initialValue}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
