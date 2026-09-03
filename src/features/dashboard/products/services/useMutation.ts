da78e3eec6abdcc9259d12e919881cae5ae2edff
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-03 12:08:36 +0330

    refactor(products): refactor mutations hook

--- src/features/dashboard/products/services/useMutation.ts
+++ src/features/dashboard/products/services/useMutation.ts
@@ -4,38 +4,45 @@ import { createNewProduct, deleteProduct, updateProduct } from "./actions";
 import { toast } from "sonner";
 import { id } from "zod/v4/locales";
 import { ProductFormValues } from "../types/schema";
+import { useRouter } from "next/navigation";
 
 export const useCreateNewProduct = () => {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: createNewProduct,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["products"] });
+      // queryClient.invalidateQueries({ queryKey: ["products"] });
+      router.refresh();
       toast.success("New product created successfully");
     },
   });
 };
 
 export const useDeleteProduct = () => {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: deleteProduct,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["products"] });
+      // queryClient.invalidateQueries({ queryKey: ["products"] });
+      router.refresh();
       toast.success("Product deleted successfully");
     },
   });
 };
 
 export const useUpdateProduct = () => {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
+  const router = useRouter();
   return useMutation({
     mutationFn: ({ id, data }: { id: string; data: ProductFormValues }) =>
       updateProduct(id, data),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["products"] });
+      // queryClient.invalidateQueries({ queryKey: ["products"] });
+      router.refresh();
       toast.success("Product updated successfully");
     },
   });
