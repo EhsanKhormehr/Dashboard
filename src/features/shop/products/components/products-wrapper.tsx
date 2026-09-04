36a3a32fad0754d0cc2b02b838af76e6b42a1acd
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-04 13:08:06 +0330

    feat(products): pass image to ProductCard to show dynamic image

--- src/features/shop/products/components/products-wrapper.tsx
+++ src/features/shop/products/components/products-wrapper.tsx
@@ -1,66 +1,7 @@
 import React from "react";
 import ProductCard from "./product-card";
-import { Button } from "@/components/ui/button";
 import { getProducts } from "../services/actions";
-
-// export const products = [
-//   {
-//     title: "Apple iPhone 15 Pro Max 256GB Natural Titanium",
-//     category: "Mobile",
-//     image: "/shop/iphone-14.png",
-//     price: 1199,
-//     oldPrice: 1299,
-//     discount: 8,
-//   },
-//   {
-//     title: "Sony WH-1000XM5 Wireless Noise Canceling Headphones",
-//     category: "Headset",
-//     image: "/shop/iphone-14.png",
-//     price: 349,
-//   },
-//   {
-//     title: "Samsung Odyssey G5 27 Inch QHD Gaming Monitor",
-//     category: "Monitor",
-//     image: "/shop/iphone-14.png",
-//     price: 260,
-//     oldPrice: 330,
-//     discount: 21,
-//   },
-//   {
-//     title: "Logitech MX Master 3S Wireless Performance Mouse",
-//     category: "Mouse",
-//     image: "/shop/iphone-14.png",
-//     price: 89,
-//   },
-//   {
-//     title: "ASUS ROG Strix G16 Gaming Laptop RTX 4060",
-//     category: "Laptop",
-//     image: "/shop/iphone-14.png",
-//     price: 1399,
-//     oldPrice: 1549,
-//     discount: 10,
-//   },
-//   {
-//     title: "Corsair Vengeance RGB 32GB DDR5 6000MHz RAM",
-//     category: "RAM",
-//     image: "/shop/iphone-14.png",
-//     price: 115,
-//   },
-//   {
-//     title: "Intel Core i7 14700K Desktop Processor",
-//     category: "CPU",
-//     image: "/shop/iphone-14.png",
-//     price: 385,
-//     oldPrice: 450,
-//     discount: 14,
-//   },
-//   {
-//     title: "Apple MacBook Air 13 Inch M3 8GB 256GB",
-//     category: "Laptop",
-//     image: "/shop/iphone-14.png",
-//     price: 999,
-//   },
-// ];
+import Pagination from "@/components/common/pagination";
 
 const ProductsWrapper = async () => {
   const products = await getProducts();
@@ -70,7 +11,7 @@ const ProductsWrapper = async () => {
         {products.map((product) => (
           <ProductCard
             category={product.category.name}
-            image={"/shop/iphone-14.png"}
+            image={product.thumbnail}
             price={product.price}
             title={product.name}
             key={product.id}
@@ -80,7 +21,6 @@ const ProductsWrapper = async () => {
         ))}
       </div>
       <div className="flex justify-center mt-8">
-        <Button className="py-5 px-8 cursor-pointer">Show More</Button>
       </div>
     </div>
   );
