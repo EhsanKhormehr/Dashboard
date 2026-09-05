62d3c57f6b52a3565792da38c004f18a5fb3dd72
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-05 19:15:09 +0330

    feat(products): pass default value to useDebouncedSearchParams hook

--- src/hooks/use-debounced-search-params.ts
+++ src/hooks/use-debounced-search-params.ts
@@ -5,17 +5,19 @@ import { useEffect, useState } from "react";
 type UseDebouncedSearchParamsProps = {
   key?: string;
   delay?: number;
+  initValue?: string | undefined;
 };
 
 export const useDebouncedSearchParams = ({
   key = "search",
   delay = 500,
+  initValue = "",
 }: UseDebouncedSearchParamsProps = {}) => {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const [searchValue, setSearchValue] = useState(searchParams.get(key) ?? "");
+  const [searchValue, setSearchValue] = useState(searchParams.get(key) ?? initValue);
   const debouncedSearch = useDebounce(searchValue, delay);
   useEffect(() => {
     const currentSearch = searchParams.get(key) ?? "";
