4467f4b3bac71c02f55a5e13802a8df34415e083
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-05 19:18:02 +0330

    feat(products): change reusable product switch structure

--- src/features/shop/products/components/products-switch-button.tsx
+++ src/features/shop/products/components/products-switch-button.tsx
@@ -5,11 +5,15 @@ import React from "react";
 type ProductsSwitchButtonProps = {
   labelValue: string;
   idValue: string;
+  onCheckedChange?: (checked: boolean) => void;
+  checked?: boolean;
 };
 
 const ProductsSwitchButton = ({
   labelValue,
   idValue,
+  onCheckedChange,
+  checked,
 }: ProductsSwitchButtonProps) => {
   return (
     <div className="flex justify-between items-center mt-4 px-2">
@@ -17,7 +21,9 @@ const ProductsSwitchButton = ({
         {labelValue}
       </Label>
       <Switch
+        onCheckedChange={onCheckedChange}
         id={idValue}
+        checked={checked}
         className="cursor-pointer !h-7 !w-12 [&>span]:bg-muted-foreground/65 [&>span]:!size-6 [&>span]:data-[state=checked]:translate-x-5 [&>span]:data-[state=checked]:bg-primary data-[state=checked]:bg-primary/30 "
       />
     </div>
