bcb6baa7d26540cd8c5bdffea5556c922e4baa4b
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-08 12:30:21 +0330

    feat(products): pass commentCount to ProductInfoPanel

--- src/features/shop/products/components/product-info-wrapper.tsx
+++ src/features/shop/products/components/product-info-wrapper.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ProductGallery from "./product-gallery";
 import ProductInfoPanel from "./product-info-panel";
 
-const ProductInfoWrapper = () => {
+type ProductInfoWrapperProps = {
+  commentCount: number;
+};
+
+const ProductInfoWrapper = ({ commentCount }: ProductInfoWrapperProps) => {
   return (
     <div className="col-span-12 lg:col-span-9">
       <div className="grid grid-cols-12 gap-5">
         <ProductGallery />
-        <ProductInfoPanel />
+        <ProductInfoPanel commentCount={commentCount} />
       </div>
     </div>
   );
