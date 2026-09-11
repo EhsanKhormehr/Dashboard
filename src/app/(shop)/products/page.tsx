fe4e09743461fa40254e91f5e756a675bb89fb4c
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-11 17:28:21 +0330

    fix: remove unsed console log

--- src/app/(shop)/products/page.tsx
+++ src/app/(shop)/products/page.tsx
@@ -1,6 +1,6 @@
 import MaxWidthWrapper from "@/components/common/max-width-wrapper";
 import Pagination from "@/components/common/pagination";
-import { getCategories } from "@/features/dashboard/products/services/actions";
+import { getCategories } from "@/features/shop/products/services/actions";
 import ProductsFiltering from "@/features/shop/products/components/products-filtering";
 import ProductsTopbarFilter from "@/features/shop/products/components/products-topbar-filter";
 import ProductsWrapper from "@/features/shop/products/components/products-wrapper";
@@ -43,7 +43,6 @@ const Products = async ({ searchParams }: ProductsProps) => {
   const products = await getProducts(normalizeUrlParams);
   const categories = await getCategories();
   const brands = await getBrands();
-  console.log(products.priceRange)
   return (
     <div>
       <MaxWidthWrapper className="grid gap-5 grid-cols-12 py-15">
