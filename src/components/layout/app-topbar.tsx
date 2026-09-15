5d5827f6e4e2cc8c70b6d863e61fe11408766d3a
EhsanKhormehr <87534015+EhsanKhormehr@users.noreply.github.com>
2026-09-15 12:38:53 +0330

    fix: fix topbar dashboard style

--- src/components/layout/app-topbar.tsx
+++ src/components/layout/app-topbar.tsx
@@ -1,8 +1,15 @@
-"use client"
+"use client";
 import React from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { ChevronDown, KeyRound, LogOut, UserRoundCog } from "lucide-react";
+import {
+  ChevronDown,
+  KeyRound,
+  LogOut,
+  PanelLeftClose,
+  PanelLeftOpen,
+  UserRoundCog,
+} from "lucide-react";
 import Image from "next/image";
 import {
   DropdownMenu,
@@ -14,32 +21,51 @@ import {
 } from "../ui/dropdown-menu";
 import Link from "next/link";
 import { useMeQuery } from "@/features/auth/me/services/useQueries";
+import { useSidebar } from "../ui/sidebar";
+import ToggleTheme from "../common/toggle-theme";
 
 export default function AppTopBar() {
-  const {data} = useMeQuery()
-
+  const { data } = useMeQuery();
+  const { toggleSidebar, open } = useSidebar();
+  console.log(open);
   return (
-    <div className="bg-surface h-[70px] w-full flex justify-end items-center">
-      <div className="mr-8 flex items-center">
-        <Image
-          src={"/Avatar.png"}
-          width={44}
-          height={44}
-          alt="avatar"
-          className="cursor-pointer"
-        />
-        <div className="mx-6">
-          <span className="text-sm font-bold block">{data?.user?.userName}</span>
-          <span className="text-xs font-semibold">{data?.user?.role}</span>
+    <div className="bg-surface h-[70px] w-full flex justify-between items-center px-3 md:px-5 lg:px-8">
+      <Button
+        variant="outline"
+        size="icon"
+        className="cursor-pointer"
+        onClick={toggleSidebar}
+      >
+        {open ? (
+          <PanelLeftClose className="size-5" />
+        ) : (
+          <PanelLeftOpen className="size-5" />
+        )}
+      </Button>
+      <div className=" flex items-center">
+        <div className="mr-6">
+          <ToggleTheme />
         </div>
+
         <DropdownMenu>
           <DropdownMenuTrigger asChild className="cursor-pointer">
-            <Button
-              variant={"outline"}
-              className="bg-white rounded-full size-[38px]"
-            >
-              <ChevronDown />
-            </Button>
+            <div className="flex items-center justify-between">
+              <Image
+                src={"/Avatar.png"}
+                width={44}
+                height={44}
+                alt="avatar"
+                className="cursor-pointer"
+              />
+              <div className="mx-6 hidden">
+                <span className="text-sm font-bold block">
+                  {data?.user?.userName}
+                </span>
+                <span className="text-xs font-semibold">
+                  {data?.user?.role}
+                </span>
+              </div>
+            </div>
           </DropdownMenuTrigger>
           <DropdownMenuContent className="w-56 shadow-card bg-surface mr-10 mt-2 border-none ">
             <DropdownMenuGroup>
